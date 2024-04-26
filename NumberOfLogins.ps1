8c0b90271936c102b2e387ac1e8bb9159d06e31d
tomaszchrulski <tomasz.chrulski@gmail.com>
2024-04-26 09:44:32 +0800

    cleanup

--- NumberOfLogins.ps1
+++ NumberOfLogins.ps1
@@ -1,6 +1,5 @@
-# Work in Progress
 
-# This .ps1 script is to be set to run at 16:30 everyday.
+# This .ps1 script is to be used to create an .exe (executable file) and set to run at 16:30 everyday.
 # The function searches for the event with ID=4624 (successful login) where the username equals a certain user within the last 24h.
 # Then, it generates an output of event date,username and workstation. At this stage it is irrelevant but I'm leaving it in.
 # The script then sends a Post request to an endpoint on ntfy.sh with information about the number of logins.
